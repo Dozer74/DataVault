c5255f384f3e2335759f28cc1e87d6eb34440b6e
Евгений <Dozer95@bk.ru>
2017-09-14 22:33:47 +0500

    add gpus & users

--- DataVault/DataModels/RenderFarm.cs
+++ DataVault/DataModels/RenderFarm.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DataVault.DataModels
 {
     using System.Data.Entity;
@@ -32,17 +34,58 @@ namespace DataVault.DataModels
         protected override void Seed(RenderFarm context)
         {
             base.Seed(context);
-            context.Users.AddRange(new[]
+
+            var users = new[]
             {
                 new User {Name = "FOX Inc.", Balance = 15000, Email = "foxArt@gmail.com"},
                 new User {Name = "Петров", Balance = 10, Email = "petya@gmail.com"},
-            });
+                new User {Name = "Иванов", Balance = 15, Email = "ivanov@ya.ru"},
+                new User {Name = "Sony Pictures", Balance = 12120, Email = "support@sony.com"},
+                new User {Name = "Warner Brothers", Balance = 25100, Email = "hello@vb.com"},
+                new User {Name = "Мегамакс", Balance = 500, Email = "mm@mm.ru"},
+                new User {Name = "Фонд кино", Balance = 1520, Email = "fond-kino@rambler.ru"},
+            };
 
-            context.GpuManufactors.AddRange(new[]
+            var gpuManufactors = new[]
             {
                 new GpuManufactor {Name = "Nvidia"},
-                new GpuManufactor {Name = "Radeon"}, 
-            });
+                new GpuManufactor {Name = "Radeon"},
+                new GpuManufactor {Name = "AMD (ATI)"},
+                new GpuManufactor {Name = "His"}
+            };
+
+            var gpus = new[]
+            {
+                new GPU {Model = "GTX 1080 ti", Manufactor = gpuManufactors[0], Passmark = Int16.MaxValue},
+                new GPU {Model = "GTX 1080", Manufactor = gpuManufactors[0], Passmark = 29000},
+                new GPU {Model = "GTX 1070", Manufactor = gpuManufactors[0], Passmark = 28500},
+                new GPU {Model = "GTX 1060", Manufactor = gpuManufactors[0], Passmark = 27000},
+                new GPU {Model = "GT 980", Manufactor = gpuManufactors[0], Passmark = 26000},
+                new GPU {Model = "GT 960", Manufactor = gpuManufactors[0], Passmark = 25000},
+                new GPU {Model = "GT 950 ti", Manufactor = gpuManufactors[0], Passmark = 23000},
+                new GPU {Model = "GT 940", Manufactor = gpuManufactors[0], Passmark = 20000},
+
+                new GPU {Model = "R9 280X", Manufactor = gpuManufactors[1], Passmark = Int16.MaxValue-550},
+                new GPU {Model = "R9 280", Manufactor = gpuManufactors[1], Passmark = 28700},
+                new GPU {Model = "R9 FURY X", Manufactor = gpuManufactors[1], Passmark = Int16.MaxValue},
+                new GPU {Model = "R7 280", Manufactor = gpuManufactors[1], Passmark = 26500},
+                new GPU {Model = "R7 270", Manufactor = gpuManufactors[1], Passmark = 25000},
+                new GPU {Model = "R7 250", Manufactor = gpuManufactors[1], Passmark = 23500},
+
+                new GPU {Model = "RX 460", Manufactor = gpuManufactors[3], Passmark = 26500-350},
+                new GPU {Model = "RX 370", Manufactor = gpuManufactors[3], Passmark = 25000-350},
+                new GPU {Model = "RX NANO", Manufactor = gpuManufactors[3], Passmark = 23500-350},
+
+
+                new GPU {Model = "C4Q", Manufactor = gpuManufactors[3], Passmark = 18350},
+                new GPU {Model = "C4V", Manufactor = gpuManufactors[3], Passmark = 17250},
+                new GPU {Model = "C2Q", Manufactor = gpuManufactors[3], Passmark = 16500},
+            };
+
+
+            context.Users.AddRange(users);
+            context.GpuManufactors.AddRange(gpuManufactors);
+            context.Gpus.AddRange(gpus);
         }
     }
 }
